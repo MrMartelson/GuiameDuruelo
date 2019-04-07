0fb23f2df1445b383f9431a99ffcf0be088acc0f
Pedro Martín <pmunnin@gmail.com>
2019-04-07 12:23:58 +0200

    Changed Map Marker
    Set a marker in Duruelo.

--- app/src/main/java/com/example/android/guiameduruelo/MapActivity.java
+++ app/src/main/java/com/example/android/guiameduruelo/MapActivity.java
@@ -32,7 +32,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker in Duruelo, Spain.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -42,8 +42,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(DurueloLat, DurueloLong);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Duruelo de la Sierra"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setMinZoomPreference(8.0f);
 

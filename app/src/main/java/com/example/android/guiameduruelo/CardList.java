94ab08b92c95b8b1998b4e4cd646b35db96238c6
Pedro Martín <pmunnin@gmail.com>
2019-04-23 13:06:39 +0200

    Created a ListView
    Created a ListView with dummy adaptator and data

--- app/src/main/java/com/example/android/guiameduruelo/CardList.java
+++ app/src/main/java/com/example/android/guiameduruelo/CardList.java
@@ -1,4 +1,32 @@
 package com.example.android.guiameduruelo;
 
-public class CardList {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class CardList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_card);
+        super.onCreate(savedInstanceState);
+
+        ListView listView = (ListView) findViewById(R.id.lv_cards);
+
+        ArrayList<String> placesList = new ArrayList<>();
+        placesList.add("Iglesia");
+        placesList.add("Casa Romulo");
+        placesList.add("Ayuntamiento");
+        placesList.add("Médico");
+        placesList.add("Castroviejo");
+
+        ArrayAdapter<String> placesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, placesList);
+
+        listView.setAdapter(placesAdapter);
+    }
 }
--- app/src/main/java/com/example/android/guiameduruelo/SectionsActivity.java
+++ app/src/main/java/com/example/android/guiameduruelo/SectionsActivity.java
@@ -1,8 +1,7 @@
 package com.example.android.guiameduruelo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,5 +22,11 @@ public class SectionsActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void seeInfo(View v){
+        Intent i = new Intent();
+        i.setClass(this, CardList.class);
+        startActivity(i);
     }
 }

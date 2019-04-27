d6ee5acad16d44d92b328c6fefea5202b47dce24
Pedro Martín <pmunnin@gmail.com>
2019-04-27 19:11:33 +0200

    Added user location

--- app/src/main/java/com/example/android/guiameduruelo/MapActivity.java
+++ app/src/main/java/com/example/android/guiameduruelo/MapActivity.java
@@ -1,9 +1,15 @@
 package com.example.android.guiameduruelo;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,20 +26,28 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MyFirebaseMsgService";
 
+    private static final int ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE = 0;
     private GoogleMap mMap;
     private double DurueloLat = 41.955181;
     private double DurueloLong = -2.931564;
     private GoogleApiClient mClient;
+    private GoogleApiClient googleApiClient;
     private Geofencing mGeofencing;
+    private Location userLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*Create the map and the toolbar*/
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = new SupportMapFragment();
@@ -46,6 +60,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        /*Set the geofence*/
         mClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -56,6 +71,13 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         mGeofencing = new Geofencing(this, mClient);
         mGeofencing.registerGeofence();
+
+        /*Check the user location*/
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     /**
@@ -72,13 +94,28 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(DurueloLat, DurueloLong);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Duruelo de la Sierra"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng duruelo = new LatLng(DurueloLat, DurueloLong);
+        mMap.addMarker(new MarkerOptions().position(duruelo).title("Duruelo de la Sierra"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(duruelo));
         mMap.setMinZoomPreference(8.0f);
 
         //Set the map over Duruelo when is created
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(DurueloLat, DurueloLong), 15.5f));
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            } else {
+                final String[] permisions = new String[]{ACCESS_FINE_LOCATION};
+                requestPermissions(permisions, ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    public void actualizeMap(GoogleMap googleMap){
     }
 
     @Override
@@ -99,17 +136,63 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
 
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                userLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            }else{
+                final String[] permisions = new String[]{ACCESS_FINE_LOCATION};
+                requestPermissions(permisions, ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            userLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);}
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                googleApiClient.reconnect();
+            }
+            if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.permission_title));
+                builder.setMessage(getString(R.string.permission_message));
+                builder.setPositiveButton(getString(R.string.acept), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final String[] permisions = new String[]{ACCESS_FINE_LOCATION};
+                        requestPermissions(permisions, ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE);
+                    }
+                });
+                builder.show();
+            }
+        }
     }
 
     @Override
     public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
